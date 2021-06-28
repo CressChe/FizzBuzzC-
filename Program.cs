038da2d502322a33a11a2acb89173c4cc0a35518
Cress <cressida.chesworth@softwire.com>
2021-06-28 11:22:54 +0100

    finished part 5

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FizzBuzzC_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 101; i++)
+            {
+                if (i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                    continue;
+                }
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                    continue;
+                }
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                    continue;
+                }
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine("Bang");
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
